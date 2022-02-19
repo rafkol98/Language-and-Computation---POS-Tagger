31aa56c99213151e49f3ee85477e28cde1676121
rafaelkoll <rafcall98@hotmail.com>
2022-02-19 16:57:08 +0000

    Emission table

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from io import open
+import numpy as np
 from conllu import parse_incr
 
 treebank = {}
@@ -29,15 +30,47 @@ test_sents = conllu_corpus(test_corpus(lang))
 print(len(train_sents), 'training sentences')
 print(len(test_sents), 'test sentences')
 
+tags2int = {
+		"<s>": 1, "ADJ": 2,"ADP": 3, "ADV": 4,"AUX": 5,"CCONJ": 6,"DET": 7,
+		"INTJ": 8,"NOUN": 9,"NUM": 10,"PART": 11,"PRON": 12,"PROPN": 13,
+		"PUNCT": 14,"SCONJ": 15,"SYM": 16,"VERB": 17, "X": 18, "</s>": 19
+	}
+
+ti = tags2int.get("ADJ")
+print(ti)
+
 # Illustration how to access the word and the part-of-speech of tokens.
-for sent in train_sents:
-	for token in sent:
-		print(token['form'], '->', token['upos'], sep='', end=' ')
-	print()
+def create_emmission_table(sents):
+	tags2int = {
+		"<s>": 1, "ADJ": 2,"ADP": 3, "ADV": 4,"AUX": 5,"CCONJ": 6,"DET": 7,
+		"INTJ": 8,"NOUN": 9,"NUM": 10,"PART": 11,"PRON": 12,"PROPN": 13,
+		"PUNCT": 14,"SCONJ": 15,"SYM": 16,"VERB": 17, "X": 18, "</s>": 19
+	}
+
+	# Create a 19 x 19 table.
+	c_table = np.zeros((len(tags2int), len(tags2int)))
+	for sent in train_sents:
+		pos_sent = getPosTagsOfSentence(sent)
+
+		for i in range(len(pos_sent)-1):
+			ti = tags2int.get(pos_sent[i])
+			ti1 = tags2int.get(pos_sent[i + 1])
+			c_table[ti][ti1] += 1
+
+	return c_table
 
 
+def getPosTagsOfSentence(sent):
+	tags = ["<s>"] # start-of-sentence marker.
 
+	# enter all the pos tags of all the words.
+	for i in range(len(sent)):
+		tags.append(sent[i]['upos'])
 
+	tags.append("/<s>") # end-of-sentence marker.
+	return tags
+
+twod_tokens = create_emmission_table(train_sents)
 
 
 
