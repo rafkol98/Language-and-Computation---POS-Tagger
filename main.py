04fcd201a04d86da0379b026b59c61a30dd41ca4
rafaelkoll <rafcall98@hotmail.com>
2022-02-23 19:16:43 +0000

    Some progress with transitions

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from io import open
 import numpy as np
+import pandas as pd
 from conllu import parse_incr
 from nltk import FreqDist, WittenBellProbDist
 
@@ -37,14 +38,22 @@ print(len(train_sents), 'training sentences')
 print(len(test_sents), 'test sentences')
 
 tags2int = {
-	"<s>": 1, "ADJ": 2, "ADP": 3, "ADV": 4, "AUX": 5, "CCONJ": 6, "DET": 7,
-	"INTJ": 8, "NOUN": 9, "NUM": 10, "PART": 11, "PRON": 12, "PROPN": 13,
-	"PUNCT": 14, "SCONJ": 15, "SYM": 16, "VERB": 17, "X": 18, "</s>": 19
+    "<s>": 1, "ADJ": 2, "ADP": 3, "ADV": 4, "AUX": 5, "CCONJ": 6, "DET": 7,
+    "INTJ": 8, "NOUN": 9, "NUM": 10, "PART": 11, "PRON": 12, "PROPN": 13,
+    "PUNCT": 14, "SCONJ": 15, "SYM": 16, "VERB": 17, "X": 18, "</s>": 19
 }
 
 ti = tags2int.get("ADJ")
 print(ti)
 
+def getTags(sents):
+    tags = []
+    for sent in sents:
+        # tags.append(Start)
+        for token in sent:
+            tags.append(token['upos']);
+
+    return tags
 
 def getPosTagsOfSentence(sent):
     tags = ["<s>"]  # start-of-sentence marker.
@@ -57,10 +66,12 @@ def getPosTagsOfSentence(sent):
     return tags
 
 
-# Illustration how to access the word and the part-of-speech of tokens.
 def create_transition_table(sents):
     # Create a 19 x 19 table.
     c_table = np.zeros((len(tags2int), len(tags2int)))
+    transition = []
+    tags = getTags(sents) # get tags.
+
     for sent in sents:
         pos_sent = getPosTagsOfSentence(sent)
 
@@ -68,34 +79,38 @@ def create_transition_table(sents):
             ti = tags2int.get(pos_sent[i], 0)
             ti1 = tags2int.get(pos_sent[i + 1], 0)
 
-            c_table[ti - 1][ti1 - 1] += 1
+            transition.append((pos_sent[i], pos_sent[i + 1]))
+            c_table[ti - 1][ti1 - 1] += 1  # increment by 1.
 
     c_table[tags2int["</s>"] - 1][tags2int["<s>"] - 1] = len(sents) - 1  # account for the transition from </s> to <s>
 
-    # TODO: have to add smoothing!!!!
-    row_sums = c_table.sum(axis=1)
-    normalized_table = c_table / row_sums[:, np.newaxis]
-    return normalized_table
+    smoothed_transition = {}
+    for tag in tags:
+        taged_tag = [t for (pt, t) in transition if pt == tag]
+        smoothed_transition[tag] = WittenBellProbDist(FreqDist(taged_tag), bins=1e5)
+
+    return c_table, smoothed_transition
 
 
-# Illustration how to access the word and the part-of-speech of tokens.
 def create_emmisions_dict(sents):
-	emissions = []
-	# TODO: method passing in sents
-	for sent in sents:
-		for token in sent:
-			emissions.append((token['upos'], token['form']))
+    emissions = []
+    for sent in sents:
+        for token in sent:
+            emissions.append((token['upos'], token['form']))
 
-	smoothed = {}
-	tags = set([t for (t, _) in emissions])
-	for tag in tags:
-		words = [w for (t, w) in emissions if t == tag]
-		smoothed[tag] = WittenBellProbDist(FreqDist(words), bins=1e5)
-
-	# print(smoothed['AUX'].prob('is')) # example of how to get the probability --> pass in word.
-
+    smoothed = {}
+    tags = set([t for (t, _) in emissions])
+    for tag in tags:
+        # print(smoothed)
+        words = [w for (t, w) in emissions if t == tag]
+        print(words)
+        smoothed[tag] = WittenBellProbDist(FreqDist(words), bins=1e5)
 
 
+# print(smoothed['AUX'].prob('is')) # example of how to get the probability --> pass in word.
 
+# emmisions = create_emmisions_dict(test_sents)
+table, transitions = create_transition_table(test_sents)
+print(transitions)
+print(transitions.get("<s>").prob('PROPN'))
 
-emmisions = create_emmisions_dict(test_sents)
